c26afe9fe95e15292266548887319db3bc207a13
yishui <727040917@qq.com>
2020-03-25 12:20:20 +0800

    封装分布式锁TryLock方法，常用于缓存失效时保护DB

--- framework/pkg/utils/lock/etcd/lock.go
+++ framework/pkg/utils/lock/etcd/lock.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/zuiqiangqishao/framework/pkg/db/etcd"
+	"github.com/zuiqiangqishao/framework/pkg/log"
 	"go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/clientv3/concurrency"
+	"math/rand"
+	"time"
 )
 
 // Exposed errors.
@@ -18,14 +21,14 @@ const (
 	GlobalPrefix = "/micro_lock/"
 )
 
-// Locker is the client for acquiring distributed locks from etcd. It should be
-// created from NewLocker() function.
-type Lock struct {
+// Conn is the DuMutex's connection and session config
+type Conn struct {
 	cli    *clientv3.Client
 	sesOpt []concurrency.SessionOption
 }
 
 type DuMutex struct {
+	conf      *Conn
 	originKey string
 	fullKey   string
 	session   *concurrency.Session
@@ -33,24 +36,49 @@ type DuMutex struct {
 	acquire   bool
 }
 
-func DefaultLock() (*Lock, error) {
+//DistributeTryLock try acquires a distributed lock  from etcd v3.
+func DistributeTryLock(c context.Context, OriginKey string, ttl int, failLockSleepMill int) (*DuMutex, error) {
 	cli, err := etcd.GetDefaultClient()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, err
 	}
-	return NewLock(cli), nil
+	conn := NewConn(cli, concurrency.WithContext(c), concurrency.WithTTL(ttl))
+	m, err := conn.NewMutex(OriginKey)
+	if err != nil {
+		e := conn.Close()
+		if e != nil {
+			err = errors.Wrap(e, " conn.NewMutex err:"+err.Error())
+		}
+		return nil, err
+	}
+	//nonblock
+	if err3 := m.TryLock(c); err3 != nil {
+		m.Release(c)
+		if errors.Cause(err) == concurrency.ErrLocked {
+			if failLockSleepMill < 0 {
+				failLockSleepMill = 300
+			}
+			delay := rand.Intn(failLockSleepMill)
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	//lock success
+	return m, nil
 }
 
-// NewLocker creates a Locker according to the given options.
-func NewLock(cli *clientv3.Client, sessionOpt ...concurrency.SessionOption) *Lock {
-	return &Lock{
+// NewConn save SessionOption and cli to create NewMutex
+func NewConn(cli *clientv3.Client, sessionOpt ...concurrency.SessionOption) *Conn {
+	return &Conn{
 		cli:    cli,
 		sesOpt: sessionOpt,
 	}
 }
 
-// Lock acquires a distributed lock for the specified resource from etcd v3.
-func (l *Lock) NewMutex(OriginKey string) (*DuMutex, error) {
+// NewMutex create a DuMutex to Lock Or TryLock
+func (l *Conn) NewMutex(OriginKey string) (*DuMutex, error) {
 	if OriginKey == "" {
 		return nil, errors.WithStack(ErrEmptyKey)
 	}
@@ -64,12 +92,22 @@ func (l *Lock) NewMutex(OriginKey string) (*DuMutex, error) {
 	m := concurrency.NewMutex(se, fullKey)
 
 	return &DuMutex{
+		conf:      l,
 		originKey: OriginKey,
 		fullKey:   fullKey,
 		session:   se,
 		mutex:     m,
 	}, nil
 
+}
+
+func (l *Conn) Close() error {
+	var err error
+	if l.cli != nil {
+		err = l.cli.Close()
+	}
+	l.cli = nil
+	return errors.WithStack(err)
 }
 
 //none block
@@ -90,8 +128,8 @@ func (d *DuMutex) Lock(c context.Context) error {
 	return errors.WithStack(err)
 }
 
-//unlock and close session
-func (d *DuMutex) Release(c context.Context) error {
+//unlock and close session and connect
+func (d *DuMutex) Release(c context.Context) {
 	var err error
 	if d.acquire {
 		err = d.mutex.Unlock(c)
@@ -100,11 +138,19 @@ func (d *DuMutex) Release(c context.Context) error {
 		err = fmt.Errorf("unlock err (%v)", err)
 	}
 
+	//close session
 	if err2 := d.session.Close(); err2 != nil {
-		err = fmt.Errorf("unlock err (%v), session close err (%v)", err, err2)
+		err = fmt.Errorf("(%v), session close err (%v)", err, err2)
 	}
 
-	return errors.WithStack(err)
+	//close clientv3 connect
+	if err3 := d.conf.Close(); err3 != nil {
+		err = fmt.Errorf(" (%v), clientv3 close err (%v)", err, err3)
+	}
+
+	if err != nil {
+		log.SugarWithContext(c).Errorf("DuMutex.Release Err(%#+v)", errors.WithStack(err))
+	}
 }
 
 func (d *DuMutex) OriginKey() string {
